42e287f055ba6ee6cfcab19822dc1d86eb5d2b00
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2018-12-03 21:29:47 +0000

    Fix Burgundy locking up randomly.
    = The issue
    
    I had a future calling wait inside of another future being run by Tokio.
    A timing issue meant it was possible for it to all lockup.
    
    = The Fix
    
    My inner future could be replaced with a stream from futures.

--- src/native_client/mod.rs
+++ src/native_client/mod.rs
@@ -23,7 +23,7 @@ pub(crate) struct NativeClient {
 
 impl NativeClient {
     pub(crate) fn new() -> Self {
-        println!("new native client");
+        info!("new native client");
         let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
         let tokio_executor = tokio_runtime.executor();
         let https = hyper_tls::HttpsConnector::new(4).unwrap();
@@ -31,7 +31,7 @@ impl NativeClient {
             .executor(tokio_executor)
             .build::<_, hyper::Body>(https);
 
-        println!("done making new native client");
+        info!("done making new native client");
 
         Self {
             client,
@@ -46,13 +46,13 @@ impl NativeClient {
         path_info: &request_information::RequestInformation,
         content: Option<String>,
     ) -> Result<response::Response, error::Error> {
-        println!("making blocking request");
+        info!("making blocking request");
         let future = self.request(method, domain_info, path_info, content)?;
 
-        println!("call blocking request");
+        info!("call blocking request");
         let response = self.tokio_runtime.block_on(future);
 
-        println!("done making blocking request");
+        info!("done making blocking request");
         response
     }
 
@@ -63,71 +63,50 @@ impl NativeClient {
         path_info: &request_information::RequestInformation,
         body_str: Option<String>,
     ) -> Result<impl Future<Item = response::Response, Error = error::Error>, error::Error> {
-        println!("make request");
+        info!("make request");
         let hyper_method = method_to_hyper(method);
         let url = request_information::to_full_url(domain_info, path_info)?;
         let mut request_builder = hyper::Request::builder();
 
-        println!("making request to {}", url);
+        info!("making request to {}", url);
         request_builder.method(hyper_method).uri(&url);
 
-        println!("set headers");
+        info!("set headers");
         domain_info.for_each_header(|(key, value)| {
-            println!("set domain header '{}' to '{}'", key, value);
+            info!("set domain header '{}' to '{}'", key, value);
             request_builder.header(key, value);
         });
         path_info.for_each_header(|(key, value)| {
-            println!("set path header '{}' to '{}'", key, value);
+            info!("set path header '{}' to '{}'", key, value);
             request_builder.header(key, value);
         });
 
-        println!("turn request into body");
+        info!("turn request into body");
         let body = content_to_body(body_str);
         let request = request_builder.body(body)?;
 
-        println!("make request future");
+        info!("make request future");
         let future = self
             .client
             .request(request)
-            .map(|res| {
-                println!("transform request to response object");
+            .and_then(|res| {
                 let status = res.status().as_u16().into();
-                let body = response_to_string(res);
-                println!("transform request to response object, has status {}", status);
+                info!("transform request to response object, with status {}", status);
 
-                response::Response {
-                    body,
-                    status,
-                }
+                res.into_body().concat2().map(move |body_chunk| {
+                    let body = String::from_utf8(body_chunk.to_vec()).unwrap();
+
+                    response::Response {
+                        body,
+                        status,
+                    }
+                })
             })
-            .map_err(|err| error::Error::from(err));
+            .map_err(error::Error::from);
 
-        println!("done making request");
+        info!("done making request");
         Ok(future)
     }
-}
-
-fn response_to_string(response: hyper::Response<hyper::body::Body>) -> String {
-    println!(">>>> response_to_string");
-    let r = response
-        .into_body()
-        .map_err(|_| ())
-        .fold(vec![], |mut acc, chunk| {
-            println!(">>>> chunking");
-            acc.extend_from_slice(&chunk);
-            println!("<<<< chunking");
-            Ok(acc)
-        })
-        .and_then(|v| {
-            println!(">>>> chunk to string");
-            let r = String::from_utf8(v).map_err(|_| ());
-            println!("<<<< chunk to string");
-            r
-        })
-        .wait()
-        .unwrap();
-    println!("<<<< response_to_string");
-    r
 }
 
 pub(crate) fn content_to_body(maybe_content: Option<String>) -> hyper::Body {
